47f0063cc864085f0d5e48548413a09d7788eb71
Bryant Biggs <bryantbiggs@gmail.com>
2019-12-30 21:00:55 -0500

    feat(cloudfront): initial implementation of CloudFront cache invalidation action

--- packages/cloudfront/index.ts
+++ packages/cloudfront/index.ts
@@ -1,19 +1,39 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as core from "@actions/core";
+import * as CloudFront from "aws-sdk/clients/cloudfront";
 
-async function run(): Promise<void> {
+async function run() {
   try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet: string = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // Inputs:
+    // The distribution's id
+    const distributionId = core.getInput("distribution-id", {
+      required: true
+    });
+    // A value to uniquely identify an invalidation request
+    // CloudFront uses the value to prevent from accidentally resubmitting an identical request
+    const callerReference = core.getInput("caller-reference", {
+      required: false
+    });
+    // A list of the paths that you want to invalidate
+    const paths = core.getInput("paths", { required: false }).split(/\r?[\n,]/);
+
+    let params = {
+      DistributionId: distributionId,
+      InvalidationBatch: {
+        CallerReference: callerReference,
+        Paths: {
+          Quantity: paths.length,
+          Items: paths
+        }
+      }
+    };
+
+    const cloudfront = new CloudFront({ apiVersion: "2019-03-26" });
+    const invalidation = await cloudfront.createInvalidation(params).promise();
+    const invalidationId = invalidation.Invalidation.Id;
+    core.setOutput("invalidation-id", invalidationId);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
--- packages/s3/index.ts
+++ packages/s3/index.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
 
 async function run(): Promise<void> {
   try {
@@ -8,9 +7,6 @@ async function run(): Promise<void> {
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
   }
