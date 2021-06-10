e82300c65249d40a4339831427b3854738c59902
Bryant Biggs <bryantbiggs@gmail.com>
2021-06-10 08:36:53 -0400

    fix(deps): update dependencies to latest and re-complile to patch reported vulnerabilities

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,12 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
-  ],
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  "rules": {
-    "prettier/prettier": ["error", { "singleQuote": true }],
-    "eol-last": ["error", "always"]
-  }
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'eol-last': ['error', 'always'],
+  },
 };
