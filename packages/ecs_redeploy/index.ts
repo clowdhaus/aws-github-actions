84f48158da5aa7df540b990717e7533305a616f6
Zsolt Fejer <zsolt.fejer@path.co.uk>
2022-04-05 17:07:25 +0100

    change param names

--- packages/ecs_redeploy/index.ts
+++ packages/ecs_redeploy/index.ts
@@ -1,11 +1,11 @@
 import * as core from '@actions/core';
-import AwsCli from '@aws-github-actions/awscli-core';
+import AwsCli from 'awscli-core';
 
 const run = async (): Promise<void> => {
   try {
     // Inputs:
-    const service = core.getInput('service ', { required: true });
-    const cluster = core.getInput('cluster', { required: true });
+    const service = core.getInput('service-name', { required: true });
+    const cluster = core.getInput('cluster-name', { required: true });
     const Aws = await AwsCli.getOrInstall();
     await Aws.call(['ecs', 'update-service', '--force-new-deployment', '--service', service, '--cluster', cluster]);
   } catch (error) {
